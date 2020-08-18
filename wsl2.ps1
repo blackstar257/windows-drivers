0ea6aa4aca2e603f912034d9bf1b011fe512294f
Kyle Umstatter <blackstar257@users.noreply.github.com>
2020-08-17 21:37:49 -0700

    Update wsl2.ps1

--- wsl2.ps1
+++ wsl2.ps1
@@ -1,3 +1,6 @@
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 wsl --set-default-version 2
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install vscode firefox saml2aws -y
